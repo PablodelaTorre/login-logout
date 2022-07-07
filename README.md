8be502a7300cc14a362a3d892969f9c08cba7882
Lauriacunia <lauriacunia@gmail.com>
2022-07-07 10:53:30 -0300

    Subo desafio de login terminado

---
+++ README.md
@@ -0,0 +1,32 @@
+### 💓👍💓 Si te fue útil éste repositorio dejame lindas valoraciones en la clase 💓👍💓
+
+### 3 proyectos hechos en la clase 24
+
+- con FileStore. Usuario
+
+```
+nmp start
+```
+
+- con FileStore. Usuario y Contador (ej con tutores)
+
+```
+npm run ejercicio
+```
+
+- con MongoAtlas.
+
+```
+npm run ejercicioMongoAtlas
+```
+
+### dependencias
+
+```
+npm i express
+npm i express-session
+npm i session-file-session
+npm i dotenv
+npm i mongoose
+npm i connect-mongo
+```
---
+++ server.js
@@ -0,0 +1,43 @@
+import express  from "express";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import 'dotenv/config'
+
+const MONGO_USER = process.env.MONGO_USER;
+const MONGO_PASS = process.env.MONGO_PASS;
+const DB_NAME = process.env.DB_NAME;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session(
+    {
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0.cyfup.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+            ttl: 60 * 10 // 10 minutes
+            })
+    }
+));
+
+app.set('views', 'src/views');
+app.set('view engine', 'ejs');
+
+
+/** Routes */
+import homeRouter from './src/routes/home.js';
+import loginRouter from './src/routes/login.js';
+import logoutRouter from './src/routes/logout.js';
+
+app.use('/', homeRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    }
+);
